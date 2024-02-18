f0aa37ef5e8f1c368e9e06d162e97e42206fb239
Shouvik <shouvikmistry24@gmail.com>
2024-02-18 15:16:00 +0530

    Updated

--- Age Calculator/script.js
+++ Age Calculator/script.js
@@ -1,16 +1,24 @@
 
-const d = new Date();
+const currentDate = new Date();
 
-document.getElementById("currDate").innerHTML = "Today date is : "+d.getDate() + "/" + (d.getMonth()+1)+"/" + d.getFullYear();
+document.getElementById("currDate").innerHTML = "Today date is : "+currentDate.getDate() + "/" + (currentDate.getMonth()+1)+"/" + currentDate.getFullYear();
 
 function getDate() {
-    let dobString = document.getElementById("myDate").value;
+    let dobString = document.getElementById("myDob").value;
     let dob = new Date(dobString);
-    let age = d.getFullYear() - dob.getFullYear();
-    document.getElementById("display").innerHTML = "Your DOB is : "+dob.getDate() + "/" + (dob.getMonth()+1)+"/" + dob.getFullYear()+"\n You are "+age+" Years old";
+    let age = currentDate.getFullYear() - dob.getFullYear();// Calculate the difference in years
+    // Check if the birthday has occurred this year
+
+    /*
+    currentDate.getMonth() < dob.getMonth(): This part checks if the current month is before the month of the person's birthday. If true, it means the birthday has not occurred yet in the current year, so the age needs adjustment.
+    (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate()): This part checks if the current month is the same as the month of the person's birthday AND if the current day of the month is before the day of the person's birthday. If both conditions are true, it means the birthday has not occurred yet in the current year, so the age needs adjustment.
+    */
+    if (currentDate.getMonth() < dob.getMonth() || (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() < dob.getDate())) {
+        age--;
+    }
     document.getElementById("age").innerHTML = "You are "+age+" Years old.";
 
-    document.getElementsByClassName("displayAge")[0].style.visibility = "visible";
+    // document.getElementsByClassName("displayAge")[0].style.visibility = "visible";
 
     document.getElementById("displayAge").style.visibility = "visible";
 }
@@ -19,15 +27,8 @@ function reloadPage() {
     // Hide the result div
     document.getElementById("displayAge").style.visibility = "hidden";
     // Reset the form by reloading the page
-    location.reload();
+    location.reload(); // method is a JavaScript method that reloads the current web page. 
 }
-
-
-//  // JavaScript code to display the current date
-//  const currentDateElement = document.getElementById('currDate');
-
-//  // Create a new Date object
-//  const currentDate = new Date();
 
 //  // Extract individual date components
 //  const year = currentDate.getFullYear();
